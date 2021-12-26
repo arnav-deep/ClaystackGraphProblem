7a0e9f672e8ec9a7b1cff3c9347853bcb10db000
arnav-deep <arnavd4@gmail.com>
2021-12-26 21:48:56 +0530

    neat message for input

--- solution.py
+++ solution.py
@@ -1,7 +1,8 @@
 from AllClassesAndFunctions import *
 
 print('Enter your input in one line separated with commas.\n' +
-        'For Example: AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7\n')
+        'For Example:\n' + 
+        'AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7\n')
 routelist = [s.strip() for s in input().strip().split(',')]
 
 g = Graph()
